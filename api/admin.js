42674481e0a700d7d7e14270d11b19a4fa583ac9
naty-cajera <natycajera1@gmail.com>
2026-01-14 18:40:29 -0300

    Update admin.js

--- api/admin.js
+++ api/admin.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   try {
     const { token, get } = req.query;
 
-    if (token !== "daiana-2026") {
+    if (token !== "naty-2026") {
       return res.status(401).json({ ok: false, error: "Token inválido" });
     }
 
